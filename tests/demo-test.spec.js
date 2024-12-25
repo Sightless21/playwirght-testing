3f1ee3ba5c0b70a8a111874d0c6e9e2637322187
Sightless21 <86817539+Sightless21@users.noreply.github.com>
2024-12-26 01:04:57 +0700

    Add Saucedemo and Youtube test cases with error

--- tests/demo-test.spec.js
+++ tests/demo-test.spec.js
@@ -2,10 +2,12 @@ const { test, expect } = require("@playwright/test");
 const exp = require("constants");
 const { skip } = require("node:test");
 
+// Saucedemo UI
 test.describe("Saucedemo test", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://www.saucedemo.com");
   });
+  //
   const email = [
     "standard_user",
     "locked_out_user",
@@ -16,6 +18,7 @@ test.describe("Saucedemo test", () => {
   ];
   const password = ["secret_sauce"];
 
+  // TC-001
   test("input field should be display as data that user enter", async ({
     page,
   }) => {
@@ -32,6 +35,7 @@ test.describe("Saucedemo test", () => {
     await expect(page.getByText(/swag labs/i)).toBeVisible();
   });
 
+  // TC-002
   test("Should show an error message if log in without an email and password", async ({
     page,
   }) => {
@@ -42,6 +46,7 @@ test.describe("Saucedemo test", () => {
     ).toBeVisible();
   });
 
+  // TC-003
   test("Should show an error message if log in without a password", async ({
     page,
   }) => {
@@ -57,6 +62,7 @@ test.describe("Saucedemo test", () => {
     ).toBeVisible();
   });
 
+  // TC-004
   test("Should show an error message if log in without an email", async ({
     page,
   }) => {
@@ -73,23 +79,102 @@ test.describe("Saucedemo test", () => {
   });
 });
 
-test.describe("Youtube test", () => {
+// Youtube
+test.describe.only("Youtube test", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://www.youtube.com/watch?v=7n6xIjTuewE");
+    await page.goto("https://www.youtube.com/watch?v=ZHgyQGoeaB0");
   });
 
-  test("should be able to click next video", async ({ page }) => {
-    await page.waitForTimeout(5500);
-    const video = await page.locator(
-      "#movie_player > div:nth-child(1) > video"
-    );
-    await video.hover({modifiers: ["Shift+N"]});
+  // TC-005
+  test.skip("should be able to click next video", async ({ page }) => {
     await page.waitForTimeout(3000);
+    const initialVideoUrl = await page.url();
+    const initialVideiTitle = await page
+      .getByRole("heading", {
+        name: "[Review] Light Shop (Spoiler Alert!)",
+        exact: true,
+      })
+      .locator("yt-formatted-string")
+      .textContent();
+    console.log(initialVideiTitle);
+    console.log(initialVideoUrl);
+    await page.keyboard.press("Shift+N");
 
-    const nextVideo = await page.getByRole("heading", {
-      name: /black and white y2k neon led lights heart background \|\| 1 hour looped hd/i,
+    // Wait for the URL to change Logic
+    try {
+      await page.waitForFunction(
+        (url) => window.location.href !== url,
+        initialVideoUrl,
+        { timeout: 5000 } // wait no longer 5 second
+      );
+    } catch (error) {
+      console.log(
+        "Timeout waiting for URL to change, using fallback timeout..."
+      );
+      await page.waitForTimeout(1000);
+    }
+    // Validate the video URL or title has changed
+    const newVideoUrl = page.url();
+    console.log(newVideoUrl);
+    await expect(newVideoUrl).not.toBe(initialVideoUrl);
+  });
+
+  // TC-006
+  test.skip("Should be able to click full screen button ", async ({ page }) => {
+    await page.waitForLoadState("networkidle"); // Wait for the page to be fully loaded
+
+    const fullScreenButton = await page.getByRole("button", {
+      name: "Full screen (f)",
     });
+    await fullScreenButton.click();
+  });
 
-    await expect(nextVideo).toBeVisible();
+  // TC-007
+  test.skip("should be able to search ", async ({ page }) => {
+    await page.waitForLoadState("networkidle"); // Wait for the page to be fully loaded
+
+    const searchInput = await page.getByRole("combobox", { name: "Search" });
+    await searchInput.fill("league of legends");
+    await searchInput.press("Enter");
+
+    const definitionLink = [
+      "https://www.youtube.com/results?search_query=league+of+legends",
+      "https://www.youtube.com/c/leagueoflegends",
+    ];
+    await page.waitForTimeout(3000);
+    expect(
+      await page.goto(
+        definitionLink[0]
+      )
+    );
+    await page.waitForTimeout(3000);
+    await page
+      .getByRole("link", {
+        name: "League of Legends tooltip Verified @leagueoflegends•15.6M subscribers Recently",
+      })
+      .click();
+    expect(await page.goto(definitionLink[1]));
+  });
+
+  // TC-008
+  test.skip('Should showing the channel information', async ({page}) => {
+    await page.waitForTimeout(3000);
+    const channelLink = await page.getByRole('link', { name: 'Goldfish Fan' })
+    await channelLink.click();
+    expect(await page.goto('https://www.youtube.com/@GoldfishFan7'))
+  });
+
+  // TC-009
+  test.skip('After press like icon in video are increased total like count', async ({page}) => {
+    await page.waitForTimeout(3000);
+    const likeButton = await page.getByRole('button', { name: /like this video along with /i })
+    await likeButton.click();
+  });
+
+  // TC-010
+  test('After press dislike icon in video are increased total dislike count', async ({page}) => {
+    await page.waitForTimeout(3000);
+    const dislikeButton = await page.getByRole('button', { name: 'Dislike this video' })
+    await dislikeButton.click();
   });
 });
